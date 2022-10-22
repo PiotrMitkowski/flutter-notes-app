67a6ea9ceab1520dc75c0d3dfb731867c85fa769
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-10-22 23:43:41 +0200

    Added localization for app title

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_notes_app/data/repository.dart';
 import 'package:flutter_notes_app/home/view/home_screen.dart';
+import 'package:flutter_notes_app/l10n/l10n.dart';
 import 'package:flutter_notes_app/styles.dart';
 
 void main() async {
@@ -28,11 +29,11 @@ class MyApp extends StatelessWidget {
     return RepositoryProvider.value(
       value: notesRepository,
       child: MaterialApp(
-        title: 'Notes app',
         theme: appTheme(),
         home: const HomeScreen(),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
+        onGenerateTitle: (context) => context.l10n.appTitle,
       ),
     );
   }
